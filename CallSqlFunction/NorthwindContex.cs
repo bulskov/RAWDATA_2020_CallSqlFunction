9e836d8dd2262c21a485bb642f19d8f68a691534
Henrik Bulskov <bulskov@ruc.dk>
2020-11-04 13:30:08 +0100

    Add project files.

---
+++ CallSqlFunction/NorthwindContex.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CallSqlFunction
+{
+    public class NorthwindContex : DbContext
+    {
+        private readonly string _connectionString;
+
+        public NorthwindContex(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DbSet<SearchResult> SearchResults { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SearchResult>().HasNoKey();
+            modelBuilder.Entity<SearchResult>().Property(x => x.Id).HasColumnName("p_id");
+            modelBuilder.Entity<SearchResult>().Property(x => x.Name).HasColumnName("p_name");
+        }
+    }
+}
---
+++ CallSqlFunction/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+namespace CallSqlFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "host=localhost;db=northwind;uid=bulskov;pwd=henrik";
+            
+            UseAdo(connectionString);
+            UseEntityFramework(connectionString);
+            UseAdoViaEntityFramework(connectionString);
+
+
+        }
+
+        private static void UseEntityFramework(string connectionString)
+        {
+            Console.WriteLine("Entity Framework");
+            var ctx = new NorthwindContex(connectionString);
+
+            //var result = ctx.SearchResults.FromSqlRaw("select * from search({0})", "%ab%");
+            var result = ctx.SearchResults.FromSqlInterpolated($"select * from search({"%ab%"})");
+
+            foreach (var searchResult in result)
+            {
+                Console.WriteLine($"{searchResult.Id}, {searchResult.Name}");
+            }
+
+        }
+
+        private static void UseAdoViaEntityFramework(string connectionString)
+        {
+            Console.WriteLine("ADO from Entity Framework");
+            var ctx = new NorthwindContex(connectionString);
+            var connection = (NpgsqlConnection) ctx.Database.GetDbConnection();
+            connection.Open();
+
+            var cmd = new NpgsqlCommand("select * from search('%ab%')", connection);
+
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader.GetInt32(0)}, {reader.GetString(1)}");
+            }
+
+        }
+
+        private static void UseAdo(string connectionString)
+        {
+            Console.WriteLine("Plain ADO");
+            var connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+
+            var cmd = new NpgsqlCommand("select * from search('%ab%')", connection);
+
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader.GetInt32(0)}, {reader.GetString(1)}");
+            }
+        }
+    }
+}
